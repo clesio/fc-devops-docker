7a85f9d7969ffe58d30796ab11c8b37611dd4860
Clesio Takahashi <clesiot@gmail.com>
2025-04-21 22:07:19 -0300

    fix: update dependencies

--- node/index.js
+++ node/index.js
@@ -10,14 +10,22 @@ const config = {
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Wesley')`
+const sql = `INSERT INTO people(name) values('Clesio')`
 connection.query(sql)
-connection.end()
+
 
 
 app.get('/', (req,res) => {
     res.send('<h1>Full Cycle</h1>')
 })
+
+//console.log('Tables in database:');
+//sql.forEach(result => {
+//  const tableName = result[Object.keys(result)[0]];
+//  console.log(tableName);
+//});
+
+connection.end()
 
 app.listen(port, ()=> {
     console.log('Rodando na porta ' + port)
